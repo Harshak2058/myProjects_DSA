be0f46ba0b1a6962c83a9cd83dbc3f4ce5f4cd3a
Harshak2058 <harshak2058@gmail.com>
2024-06-25 15:25:42 +0530

    bubbleSort

---
+++ bubblesort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void bubbleSort(int arr[], int n)
+{
+ for(int i=0;i<n-1;i++)
+ {
+    for(int j=0;j<n-i-1;j++)
+    {
+        if(arr[j]>arr[j+1]){
+             int temp=arr[j];
+             arr[j]=arr[j+1];
+             arr[j+1]=temp;
+        }
+    }
+ }
+}
+void bubbleArray(int arr[],int size)
+{
+    for(int i=0;i<size;i++)
+    {cout<<arr[i]<<"\t";
+    }
+}
+
+int main()
+{
+    int n=5;
+    int arr[n]={1,4,3,6,5};
+    bubbleSort(arr,n);
+  bubbleArray(arr,n);
+
+    return 0;
+}
