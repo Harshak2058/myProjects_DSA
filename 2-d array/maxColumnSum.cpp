47052fd3dbd434ec8b3fa28a4914fb7d146a9ea7
Harshak2058 <harshak2058@gmail.com>
2024-06-27 14:56:15 +0530

    maxRowSum

--- 2-d array/maxColumnSum.cpp
+++ 2-d array/maxColumnSum.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int arr[3][4];
-     int sum=0,max=0;
+     int sum=0;
     
   //  int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
     for(int i=0;i<3;i++)
@@ -20,22 +20,17 @@ int main()
         for(int j=0;j<4;j++){
            
             sum+=arr[i][j];
-            if(max<sum)
-            max=sum;
             if(j==3){
             cout<<"sum is:"<<sum<<" "<<endl;
             sum =0;
             i++;
             if(i==3) break;
             
-            
             goto label;
             }
 
-        }
-        cout<<"max is : "<<max;
     }
-
+    }
    // cout<<"sum is:"<<sum<<" ";
     cout<<endl;
     return 0;
